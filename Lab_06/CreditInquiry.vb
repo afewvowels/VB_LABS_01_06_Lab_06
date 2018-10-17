61d112e3482799005142f76f739027c080f2248b
afewvowels <keithbsmith87@gmail.com>
2018-10-17 12:58:30 -0400

    initial commit

---
+++ Lab_06/CreditInquiry.vb
@@ -0,0 +1,12 @@
+﻿'Project: StreamReader
+'Author: Anthony DePinto 
+'Date: Fall 2014 
+'Description: 
+' Read a file sequentially and display contents based on
+' account type specified by user (credit, debit or zero balances).
+Imports System.IO ' using classes from this namespace
+
+Public Class CreditInquiry
+    
+End Class ' Credit Inquiry
+
